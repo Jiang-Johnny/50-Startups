515a9874fb9cadf4970931edbbb19bee3ea8d511
Johnny Jiang <130807463+Jiang-Johnny@users.noreply.github.com>
2023-06-17 18:19:18 -0400

    Update Machine Learning Linear Regression.py

--- Machine Learning Linear Regression.py
+++ Machine Learning Linear Regression.py
@@ -1,1 +1,10 @@
+#Importing Libraries
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
 
