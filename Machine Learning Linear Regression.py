f6acb984f73c1499462976566e536cbdac0a49b1
Johnny Jiang <130807463+Jiang-Johnny@users.noreply.github.com>
2023-06-18 15:46:32 -0400

    Update Machine Learning Linear Regression.py

--- Machine Learning Linear Regression.py
+++ Machine Learning Linear Regression.py
@@ -1,15 +1,48 @@
-#Importing Libraries
+#Importing libraries
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import sklearn
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
 
-#reading the data 
+#Reading the data 
 
 df = pd.read_csv('/content/drive/MyDrive/Datasets/50_Startups.csv')
 df.head()
+
+#Using heatmap to see the correlations between each attribute
+
+sns.heatmap(df.corr(),annot=True)
+plt.show()
+
+#Running a linear regression machine learning algorithm for accuracy
+
+df2=df[['R&D Spend','Administration','Marketing Spend', 'Profit']]
+
+predict = 'Profit'
+
+x= np.array(df2.drop([predict],1))
+y=np.array(df[predict])
+
+x_train,x_test,y_train,y_test = sklearn.model_selection.train_test_split(x,y,test_size=0.2)
+
+lr = LinearRegression()
+
+lr.fit(x_train,y_train)
+acc = lr.score(x_test,y_test)
+print(acc)
+
+#Print out the Linear Regression coefficients and the intercept
+
+print("Coefficient:", lr.coef_)
+print("Intercept", lr.intercept_)
+
+#Using the machine learning algorithm to making predicitions.
+
+predictions = lr.predict(x_test)
+
+for i in range(len(predictions)):
+  print(predictions[i],x_test[i],y_test[i])
 
 
